f8b9d375768fd5c3b607f4e2f07423309bda0220
ValdikSS <iam@valdikss.org.ru>
2025-12-24 01:33:08 +0300

    Fix printing waiting for LBP1120
    LBP1120 does not set PAPER_DELIVERY right after sending video
    data, which makes printing impossible: the printer is released
    before it manage to print anything.

--- libcapt/Protocol/ExtendedStatus.hpp
+++ libcapt/Protocol/ExtendedStatus.hpp
@@ -23,7 +23,8 @@ namespace Capt::Protocol {
         }
 
         constexpr bool IsPrinting() const noexcept {
-            return (this->Aux & AuxStatus::PAPER_DELIVERY) != 0 || (this->Aux & AuxStatus::SAFE_TIMER) != 0;
+            return (this->Aux & AuxStatus::PAPER_DELIVERY) != 0 || (this->Aux & AuxStatus::SAFE_TIMER) != 0
+                || !this->Printed;
         }
 
         constexpr bool Ready() const noexcept {
